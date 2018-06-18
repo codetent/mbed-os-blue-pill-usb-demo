e0b6d6b32781864c021c4791215c30d6b79fbf13
Christoph Swoboda <christophsw@gmail.com>
2018-06-18 18:43:53 +0200

    submodule added

--- BUILD/BLUEPILL_F103C8/GCC_ARM/main.d
+++ BUILD/BLUEPILL_F103C8/GCC_ARM/main.d
@@ -1,4 +1,4 @@
-BUILD\BLUEPILL_F103C8\GCC_ARM\.\main.o: main.cpp \
+BUILD\BLUEPILL_F103C8\GCC_ARM\main.o: main.cpp \
  BUILD\BLUEPILL_F103C8\GCC_ARM\mbed_config.h \
  mbed-os/targets/TARGET_STM/USBLibs/USBSerial/USBSerial.h \
  mbed-os/targets/TARGET_STM/USBLibs/USBSerial/USBCDC.h \
@@ -7,15 +7,19 @@ BUILD\BLUEPILL_F103C8\GCC_ARM\.\main.o: main.cpp \
  mbed-os/targets/TARGET_STM/USBLibs/USBDevice/USBDescriptor.h \
  mbed-os/targets/TARGET_STM/USBLibs/USBDevice/USBDevice_Types.h \
  mbed-os/targets/TARGET_STM/USBLibs/USBDevice/USBDevice.h mbed-os/mbed.h \
- mbed-os/rtos/rtos.h mbed-os/targets/TARGET_STM/mbed_rtx.h \
- mbed-os/rtos/rtx5/mbed_rtx_conf.h mbed-os/rtos/mbed_rtos_storage.h \
- mbed-os/rtos/rtx5/TARGET_CORTEX_M/rtx_lib.h \
- mbed-os/rtos/rtx5/TARGET_CORTEX_M/core_cm.h \
+ mbed-os/rtos/rtos.h mbed-os/rtos/TARGET_CORTEX/mbed_rtos_storage.h \
+ mbed-os/rtos/TARGET_CORTEX/rtx5/RTX/Source/rtx_lib.h \
+ mbed-os/rtos/TARGET_CORTEX/rtx5/RTX/Source/rtx_core_c.h \
+ mbed-os/cmsis/RTE_Components.h \
+ mbed-os/rtos/TARGET_CORTEX/mbed_rtx_conf.h \
+ mbed-os/targets/TARGET_STM/mbed_rtx.h mbed-os/cmsis/mbed_cmsis_conf.h \
  mbed-os/targets/TARGET_STM/TARGET_STM32F1/TARGET_BLUEPILL_F103C8/device/cmsis.h \
  mbed-os/targets/TARGET_STM/TARGET_STM32F1/TARGET_BLUEPILL_F103C8/device/stm32f1xx.h \
  mbed-os/targets/TARGET_STM/TARGET_STM32F1/TARGET_BLUEPILL_F103C8/device/stm32f103xb.h \
- mbed-os/cmsis/core_cm3.h mbed-os/cmsis/TARGET_CORTEX_M/cmsis_compiler.h \
- mbed-os/cmsis/TARGET_CORTEX_M/TOOLCHAIN_GCC/cmsis_gcc.h \
+ mbed-os/cmsis/TARGET_CORTEX_M/core_cm3.h \
+ mbed-os/cmsis/TARGET_CORTEX_M/cmsis_version.h \
+ mbed-os/cmsis/TARGET_CORTEX_M/cmsis_compiler.h \
+ mbed-os/cmsis/TARGET_CORTEX_M/cmsis_gcc.h \
  mbed-os/targets/TARGET_STM/TARGET_STM32F1/TARGET_BLUEPILL_F103C8/device/system_stm32f1xx.h \
  mbed-os/targets/TARGET_STM/TARGET_STM32F1/device/stm32f1xx_hal.h \
  mbed-os/targets/TARGET_STM/TARGET_STM32F1/device/stm32f1xx_hal_conf.h \
@@ -64,39 +68,45 @@ BUILD\BLUEPILL_F103C8\GCC_ARM\.\main.o: main.cpp \
  mbed-os/targets/TARGET_STM/TARGET_STM32F1/device/stm32f1xx_hal_hcd.h \
  mbed-os/targets/TARGET_STM/TARGET_STM32F1/device/stm32f1xx_hal_mmc.h \
  mbed-os/targets/TARGET_STM/TARGET_STM32F1/TARGET_BLUEPILL_F103C8/device/cmsis_nvic.h \
- mbed-os/cmsis/arm_math.h mbed-os/cmsis/core_cm3.h \
- mbed-os/cmsis/tz_context.h mbed-os/rtos/rtx5/TARGET_CORTEX_M/cmsis_os2.h \
- mbed-os/rtos/rtx5/TARGET_CORTEX_M/rtx_os.h \
- mbed-os/rtos/rtx5/TARGET_CORTEX_M/rtx_evr.h \
- mbed-os/rtos/rtx5/TARGET_CORTEX_M/RTX_Config.h \
- mbed-os/rtos/rtx5/mbed_rtx_conf.h mbed-os/rtos/Thread.h \
- mbed-os/rtos/rtx5/TARGET_CORTEX_M/cmsis_os2.h \
- mbed-os/rtos/mbed_rtos1_types.h mbed-os/rtos/rtx4/cmsis_os.h \
- mbed-os/rtos/rtx5/TARGET_CORTEX_M/rtx_os.h mbed-os/platform/Callback.h \
+ mbed-os/rtos/TARGET_CORTEX/rtx5/RTX/Source/rtx_core_cm.h \
+ mbed-os/rtos/TARGET_CORTEX/rtx5/Include/os_tick.h \
+ mbed-os/rtos/TARGET_CORTEX/rtx5/Include/cmsis_os2.h \
+ mbed-os/rtos/TARGET_CORTEX/rtx5/RTX/Config/RTX_Config.h \
+ mbed-os/rtos/TARGET_CORTEX/rtx5/RTX/Include/rtx_os.h \
+ mbed-os/rtos/TARGET_CORTEX/rtx5/RTX/Include/rtx_evr.h \
+ mbed-os/rtos/TARGET_CORTEX/rtx5/RTX/Include/rtx_os.h \
+ mbed-os/rtos/TARGET_CORTEX/mbed_rtx_conf.h mbed-os/rtos/Kernel.h \
+ mbed-os/rtos/Thread.h mbed-os/rtos/TARGET_CORTEX/mbed_rtos1_types.h \
+ mbed-os/rtos/TARGET_CORTEX/rtx4/cmsis_os.h mbed-os/platform/Callback.h \
  mbed-os/platform/mbed_assert.h mbed-os/platform/mbed_preprocessor.h \
  mbed-os/platform/mbed_toolchain.h mbed-os/platform/NonCopyable.h \
- mbed-os/rtos/Semaphore.h mbed-os/rtos/Mutex.h mbed-os/rtos/RtosTimer.h \
+ mbed-os/rtos/Semaphore.h mbed-os/rtos/Mutex.h \
+ mbed-os/platform/ScopedLock.h mbed-os/rtos/RtosTimer.h \
  mbed-os/rtos/Mail.h mbed-os/rtos/Queue.h mbed-os/platform/mbed_error.h \
- mbed-os/rtos/MemoryPool.h mbed-os/rtos/MemoryPool.h mbed-os/rtos/Queue.h \
+ mbed-os/platform/mbed_retarget.h mbed-os/rtos/MemoryPool.h \
+ mbed-os/rtos/MemoryPool.h mbed-os/rtos/Queue.h mbed-os/rtos/EventFlags.h \
+ mbed-os/rtos/ConditionVariable.h \
+ mbed-os/rtos/TARGET_CORTEX/rtx4/cmsis_os.h \
  mbed-os/features/netsocket/nsapi.h \
  mbed-os/features/netsocket/nsapi_types.h \
  mbed-os/features/netsocket/SocketAddress.h \
  mbed-os/platform/mbed_toolchain.h \
  mbed-os/features/netsocket/NetworkStack.h \
  mbed-os/features/netsocket/NetworkInterface.h \
- mbed-os/features/netsocket/nsapi_types.h \
+ mbed-os/features/netsocket/nsapi_types.h mbed-os/platform/Callback.h \
+ mbed-os/features/netsocket/DNS.h \
  mbed-os/features/netsocket/EthInterface.h \
  mbed-os/features/netsocket/WiFiInterface.h \
  mbed-os/features/netsocket/WiFiAccessPoint.h \
- mbed-os/features/netsocket/CellularInterface.h \
+ mbed-os/features/netsocket/CellularBase.h \
  mbed-os/features/netsocket/MeshInterface.h \
- mbed-os/features/netsocket/Socket.h mbed-os/platform/Callback.h \
+ mbed-os/features/netsocket/Socket.h \
  mbed-os/features/netsocket/UDPSocket.h \
  mbed-os/features/netsocket/TCPSocket.h \
  mbed-os/features/netsocket/TCPServer.h \
  mbed-os/features/netsocket/nsapi_ppp.h mbed-os/platform/FileHandle.h \
  mbed-os/platform/Callback.h mbed-os/platform/mbed_poll.h \
- mbed-os/platform/platform.h mbed-os/platform/mbed_retarget.h \
+ mbed-os/platform/platform.h \
  mbed-os/targets/TARGET_STM/TARGET_STM32F1/device.h \
  mbed-os/targets/TARGET_STM/TARGET_STM32F1/TARGET_BLUEPILL_F103C8/objects.h \
  mbed-os/targets/TARGET_STM/PortNames.h \
@@ -111,6 +121,7 @@ BUILD\BLUEPILL_F103C8\GCC_ARM\.\main.o: main.cpp \
  mbed-os/features/netsocket/NetworkStack.h mbed-os/events/mbed_events.h \
  mbed-os/events/equeue/equeue.h mbed-os/events/equeue/equeue_platform.h \
  mbed-os/events/EventQueue.h mbed-os/events/Event.h \
+ mbed-os/events/mbed_shared_queues.h \
  mbed-os/features/filesystem/mbed_filesystem.h \
  mbed-os/features/filesystem/FileSystem.h mbed-os/platform/FileBase.h \
  mbed-os/platform/SingletonPtr.h mbed-os/platform/PlatformMutex.h \
@@ -136,7 +147,9 @@ BUILD\BLUEPILL_F103C8\GCC_ARM\.\main.o: main.cpp \
  mbed-os/drivers/PortInOut.h mbed-os/drivers/PortOut.h \
  mbed-os/drivers/AnalogIn.h mbed-os/hal/analogin_api.h \
  mbed-os/drivers/AnalogOut.h mbed-os/drivers/PwmOut.h \
- mbed-os/hal/pwmout_api.h mbed-os/drivers/Serial.h \
+ mbed-os/hal/pwmout_api.h mbed-os/platform/mbed_power_mgmt.h \
+ mbed-os/hal/sleep_api.h mbed-os/platform/mbed_toolchain.h \
+ mbed-os/hal/ticker_api.h mbed-os/drivers/Serial.h \
  mbed-os/platform/Stream.h mbed-os/platform/FileLike.h \
  mbed-os/drivers/SerialBase.h mbed-os/hal/serial_api.h \
  mbed-os/hal/buffer.h mbed-os/hal/dma_api.h \
@@ -150,19 +163,21 @@ BUILD\BLUEPILL_F103C8\GCC_ARM\.\main.o: main.cpp \
  mbed-os/hal/serial_api.h mbed-os/drivers/UARTSerial.h \
  mbed-os/drivers/InterruptIn.h mbed-os/hal/gpio_irq_api.h \
  mbed-os/platform/CircularBuffer.h mbed-os/drivers/FlashIAP.h \
- mbed-os/drivers/Timer.h mbed-os/hal/ticker_api.h \
- mbed-os/drivers/Ticker.h mbed-os/drivers/TimerEvent.h \
- mbed-os/hal/us_ticker_api.h mbed-os/drivers/Timeout.h \
+ mbed-os/drivers/MbedCRC.h mbed-os/drivers/TableCRC.h \
+ mbed-os/hal/crc_api.h mbed-os/drivers/Timer.h mbed-os/drivers/Ticker.h \
+ mbed-os/drivers/TimerEvent.h mbed-os/hal/us_ticker_api.h \
+ mbed-os/hal/lp_ticker_api.h mbed-os/drivers/Timeout.h \
  mbed-os/drivers/Ticker.h mbed-os/drivers/LowPowerTimeout.h \
  mbed-os/drivers/LowPowerTicker.h mbed-os/drivers/LowPowerTimer.h \
  mbed-os/drivers/Timer.h mbed-os/platform/LocalFileSystem.h \
  mbed-os/drivers/InterruptIn.h mbed-os/platform/mbed_wait_api.h \
- mbed-os/hal/sleep_api.h mbed-os/platform/mbed_sleep.h \
- mbed-os/hal/sleep_api.h mbed-os/platform/mbed_rtc_time.h \
- mbed-os/platform/mbed_poll.h mbed-os/platform/ATCmdParser.h \
- mbed-os/platform/FileSystemHandle.h mbed-os/platform/FileHandle.h \
- mbed-os/platform/DirHandle.h mbed-os/platform/Callback.h \
- mbed-os/platform/FunctionPointer.h \
+ mbed-os/hal/sleep_api.h mbed-os/platform/mbed_power_mgmt.h \
+ mbed-os/platform/mbed_rtc_time.h mbed-os/platform/mbed_poll.h \
+ mbed-os/platform/ATCmdParser.h mbed-os/platform/FileSystemHandle.h \
+ mbed-os/platform/FileHandle.h mbed-os/platform/DirHandle.h \
+ mbed-os/platform/CriticalSectionLock.h mbed-os/platform/DeepSleepLock.h \
+ mbed-os/platform/mbed_stats.h mbed-os/platform/Callback.h \
+ mbed-os/platform/FunctionPointer.h mbed-os/platform/ScopedLock.h \
  mbed-os/targets/TARGET_STM/USBLibs/USBDevice/USBDevice_Types.h \
  mbed-os/targets/TARGET_STM/USBLibs/USBDevice/USBHAL.h \
  mbed-os/targets/TARGET_STM/USBLibs/USBDevice/USBEndpoints.h \
